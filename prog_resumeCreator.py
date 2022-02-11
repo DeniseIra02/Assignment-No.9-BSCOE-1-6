34da5274353e765a082ac8c2e971a16e2bf48b64
DeniseIra02 <irajubilo@gmail.com>
2022-02-11 20:16:33 +0800

    Complete program and enhancements are successfully added.

--- prog_resumeCreator.py
+++ prog_resumeCreator.py
@@ -37,16 +37,17 @@ place = raw_details["place"]
 year_2 = raw_details["year2"]
 
 grad = raw_details["_grad"]
-summary = raw_details["_summary"]
+summary1 = raw_details["summary_s1"]
+summary2 = raw_details["summary_s2"]
 
 #PDF 
 #headerandformat
-_resume = FPDF('P', 'cm', 'Legal')
+_resume = FPDF('P', 'cm', 'A4')
 _resume.add_page()
 _resume.set_margins(0,0,0)
 _resume.set_font("helvetica", "B", 25)
-_resume.set_text_color(0,0,102)
-_resume.set_fill_color(255,255,153)
+_resume.set_text_color(0,0,0)
+_resume.set_fill_color(102,178,255)
 _resume.set_draw_color(0,0,102)
 _resume.cell(0,4.5, name, align='C', ln=2, fill=1, border='LTB')
 _resume.image('2x2.jpg',0,1,4.5)
@@ -58,103 +59,142 @@ _resume.cell(0,0.3, ln=1, fill=1)
 _resume.set_font("helvetica", "B", 14)
 _resume.set_text_color(255,255,255)
 _resume.set_fill_color(0,0,102)
-_resume.cell(0,0.5, "          Personal Information", align='L', ln=1, fill=1, border='TB')
-
-_resume.set_font("helvetica", "", 11)
-_resume.set_text_color(0,0,0)
-_resume.set_fill_color(255,255,255)
-_resume.set_draw_color(0,0,0)
-_resume.cell(0,0.7, "                    Sex: " + sex, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                    Age: " + age, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                    Address: " + address, align='L', ln=1, fill=1)
+_resume.cell(0,0.7, "          Personal Information", align='L', ln=1, fill=1, border='TB')
 
 _resume.set_fill_color(255,255,255)
 _resume.cell(0,0.1, ln=1, fill=1)
 
-_resume.set_font("helvetica", "B", 14)
-_resume.set_text_color(255,255,255)
-_resume.set_fill_color(0,0,102)
-_resume.cell(0,0.5, "          Contact Information", align='L', ln=1, fill=1, border='TB')
-
 _resume.set_font("helvetica", "", 11)
 _resume.set_text_color(0,0,0)
 _resume.set_fill_color(255,255,255)
 _resume.set_draw_color(0,0,0)
-_resume.cell(0,0.7, "                    Contact Number: " + phone, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                    Email Address: " + email, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                    Sex: " + sex, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                    Age: " + age, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                    Address: " + address, align='L', ln=1, fill=1)
+
+_resume.set_fill_color(255,255,255)
+_resume.cell(0,0.2, ln=1, fill=1)
+
+_resume.set_font("helvetica", "B", 14)
+_resume.set_text_color(255,255,255)
+_resume.set_fill_color(0,0,102)
+_resume.cell(0,0.7, "          Contact Information", align='L', ln=1, fill=1, border='TB')
 
 _resume.set_fill_color(255,255,255)
 _resume.cell(0,0.1, ln=1, fill=1)
 
-_resume.set_font("helvetica", "B", 14)
-_resume.set_text_color(255,255,255)
-_resume.set_fill_color(0,0,102)
-_resume.cell(0,0.5, "          Accounts", align='L', ln=1, fill=1, border='TB')
-
 _resume.set_font("helvetica", "", 11)
 _resume.set_text_color(0,0,0)
 _resume.set_fill_color(255,255,255)
 _resume.set_draw_color(0,0,0)
-_resume.cell(0,0.7, "                    Network: " + network_1, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + username_1, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + link_1, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                    Network: " + network_2, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + username_2, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + link_2, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                    Contact Number: " + phone, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                    Email Address: " + email, align='L', ln=1, fill=1)
+
+_resume.set_fill_color(255,255,255)
+_resume.cell(0,0.2, ln=1, fill=1)
+
+_resume.set_font("helvetica", "B", 14)
+_resume.set_text_color(255,255,255)
+_resume.set_fill_color(0,0,102)
+_resume.cell(0,0.7, "          Accounts", align='L', ln=1, fill=1, border='TB')
 
 _resume.set_fill_color(255,255,255)
 _resume.cell(0,0.1, ln=1, fill=1)
 
-_resume.set_font("helvetica", "B", 14)
-_resume.set_text_color(255,255,255)
-_resume.set_fill_color(0,0,102)
-_resume.cell(0,0.5, "          Education", align='L', ln=1, fill=1, border='TB')
-
 _resume.set_font("helvetica", "", 11)
 _resume.set_text_color(0,0,0)
 _resume.set_fill_color(255,255,255)
 _resume.set_draw_color(0,0,0)
-_resume.cell(0,0.7, "                                   " + course, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + university, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + year_1, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                    Network: " + network_1, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + username_1, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + link_1, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                    Network: " + network_2, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + username_2, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + link_2, align='L', ln=1, fill=1)
+
+_resume.set_fill_color(255,255,255)
+_resume.cell(0,0.2, ln=1, fill=1)
+
+_resume.set_font("helvetica", "B", 14)
+_resume.set_text_color(255,255,255)
+_resume.set_fill_color(0,0,102)
+_resume.cell(0,0.7, "          Education", align='L', ln=1, fill=1, border='TB')
 
 _resume.set_fill_color(255,255,255)
 _resume.cell(0,0.1, ln=1, fill=1)
 
-_resume.set_font("helvetica", "B", 14)
-_resume.set_text_color(255,255,255)
-_resume.set_fill_color(0,0,102)
-_resume.cell(0,0.5, "          Skills", align='L', ln=1, fill=1, border='TB')
-
 _resume.set_font("helvetica", "", 11)
 _resume.set_text_color(0,0,0)
 _resume.set_fill_color(255,255,255)
 _resume.set_draw_color(0,0,0)
-_resume.cell(0,0.7, "                                   " + skill_1, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + skill_2, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + skill_3, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + skill_4, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + skill_5, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + skill_6, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                   " + skill_7, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + course, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + university, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + year_1, align='L', ln=1, fill=1)
+
+_resume.set_fill_color(255,255,255)
+_resume.cell(0,0.2, ln=1, fill=1)
+
+_resume.set_font("helvetica", "B", 14)
+_resume.set_text_color(255,255,255)
+_resume.set_fill_color(0,0,102)
+_resume.cell(0,0.7, "          Skills", align='L', ln=1, fill=1, border='TB')
 
 _resume.set_fill_color(255,255,255)
 _resume.cell(0,0.1, ln=1, fill=1)
 
+_resume.set_font("helvetica", "", 11)
+_resume.set_text_color(0,0,0)
+_resume.set_fill_color(255,255,255)
+_resume.set_draw_color(0,0,0)
+_resume.cell(0,0.5, "                                   " + skill_1, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + skill_2, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + skill_3, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + skill_4, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + skill_5, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + skill_6, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + skill_7, align='L', ln=1, fill=1)
+
+_resume.set_fill_color(255,255,255)
+_resume.cell(0,0.2, ln=1, fill=1)
+
 _resume.set_font("helvetica", "B", 14)
 _resume.set_text_color(255,255,255)
 _resume.set_fill_color(0,0,102)
-_resume.cell(0,0.5, "          Experience", align='L', ln=1, fill=1, border='TB')
+_resume.cell(0,0.7, "          Experience", align='L', ln=1, fill=1, border='TB')
+
+_resume.set_fill_color(255,255,255)
+_resume.cell(0,0.1, ln=1, fill=1)
 
 _resume.set_font("helvetica", "", 11)
 _resume.set_text_color(0,0,0)
 _resume.set_fill_color(255,255,255)
 _resume.set_draw_color(0,0,0)
-_resume.cell(0,0.7, "                    On-the-Job Training: ", align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                         " + ojt, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                         " + company, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                         " + place, align='L', ln=1, fill=1)
-_resume.cell(0,0.7, "                                         " + year_2, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                    On-the-Job Training: ", align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + ojt, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + company, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + place, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                                   " + year_2, align='L', ln=1, fill=1)
+
+_resume.set_fill_color(255,255,255)
+_resume.cell(0,0.2, ln=1, fill=1)
+
+_resume.set_font("helvetica", "B", 14)
+_resume.set_text_color(255,255,255)
+_resume.set_fill_color(0,0,102)
+_resume.cell(0,0.7, "          Summary", align='L', ln=1, fill=1, border='TB')
+
+_resume.set_font("helvetica", "B", 11)
+_resume.set_text_color(0,0,0)
+_resume.set_fill_color(255,255,255)
+_resume.set_draw_color(0,0,0)
+_resume.cell(0,1, "                    " + grad, align='L', ln=1, fill=1)
+
+_resume.set_font("helvetica", "", 11)
+_resume.set_text_color(0,0,0)
+_resume.set_fill_color(255,255,255)
+_resume.set_draw_color(0,0,0)
+_resume.cell(0,0.5, "                                " + summary1, align='L', ln=1, fill=1)
+_resume.cell(0,0.5, "                    " + summary2, align='L', ln=1, fill=1)
 
 #output
 _resume.output("JUBILO_DENISEIRA.pdf")
